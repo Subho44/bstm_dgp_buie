b663007418a186a2fb1ce3eef8ab5d598ac36159
Subho44 <98902921+Subho44@users.noreply.github.com>
2025-07-21 11:27:50 +0530

    authentication
    done

--- Eco-smart-clean-city/server/server.js
+++ Eco-smart-clean-city/server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const areaRoutes = require('./routes/areaRoutes');
+const authRoutes = require('./routes/authRoutes');
 const connectdb = require ('./config/db')
 dotenv.config();
 connectdb();
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/uploads',express.static('uploads'));
 app.use('/api/areas',areaRoutes);
+app.use('/api/auth',authRoutes);
 
 const PORT = process.env.PORT || 5600;
 app.listen(PORT,()=>console.log("server is running port 5600"));
